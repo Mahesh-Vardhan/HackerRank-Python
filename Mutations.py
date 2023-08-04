8b2649257224b1158ba3b6f0b4fe8ad11d3a8159
Akena Mahesh Vardhan <113368513+Mahesh-Vardhan@users.noreply.github.com>
2023-08-04 08:57:22 +0530

    String Mutations
    The problem involves replacing a character in a given string with a new character at a specified position. The provided Python code defines a function mutate_string, but it's initially incomplete. The complete code takes a string, position, and character as input, replaces the character, and outputs the updated string.

---
+++ Mutations.py
@@ -0,0 +1,16 @@
+def mutate_string(string, position, character):
+    # Check if the position is within the valid range of the string
+    if position < 1 or position > len(string):
+        return "Invalid position."
+
+    # Construct the new string by replacing the character at the specified position
+    new_string = string[:position - 1] + character + string[position:]
+
+    return new_string
+
+if __name__ == '__main__':
+    s = input("Enter the string: ")
+    i, c = input("Enter position and character: ").split()
+
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
