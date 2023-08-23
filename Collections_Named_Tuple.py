a9c9e0dd847fcf09c259843f75ec1b47dbc8c825
Akena Mahesh Vardhan <113368513+Mahesh-Vardhan@users.noreply.github.com>
2023-08-23 18:57:17 +0530

    collections.namedtuple()

---
+++ Collections_Named_Tuple.py
@@ -0,0 +1,11 @@
+N = int(input())
+names = input().split()
+index_num = names.index("MARKS")
+marks = 0
+for _ in range(0, N):
+    Data_Input = input().split()
+    Data_Input[index_num] = int(Data_Input[index_num])
+    marks = marks+Data_Input[index_num]
+Avg_marks = marks/N
+rounded_avg = "{:.2f}".format(Avg_marks)
+print(rounded_avg)
